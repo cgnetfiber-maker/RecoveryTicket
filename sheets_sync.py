0ecff06414df0eb3203ef11a4bdf96677d3b3ff2
cgnetfiber-maker <cgnetfiber@gmail.com>
2026-09-23 17:00:45 +0545

    Update sheets_sync.py

--- sheets_sync.py
+++ sheets_sync.py
@@ -1,19 +1,35 @@
 import os
 import json
 import gspread
+import pandas as pd
 from google.oauth2.service_account import Credentials
+from gspread_dataframe import set_with_dataframe
 
-# 1. Fetch values from Render Environment Variables
-SPREADSHEET_ID = os.getenv("GOOGLE_SPREADSHEET_ID", "1SyhE9nQf3LV8QEBTCLavgfr8w45y-mI69XPbtuN2hPA")
-WORKSHEET_GID = os.getenv("GOOGLE_WORKSHEET_GID", "84560511")
-SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+def upload_dataframe_to_sheet(df: pd.DataFrame):
+    """Uploads a pandas DataFrame to Google Sheets using environment variables."""
+    # Read environment variables
+    spreadsheet_id = os.getenv("GOOGLE_SPREADSHEET_ID")
+    worksheet_gid = os.getenv("GOOGLE_WORKSHEET_GID")
+    service_account_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 
-# 2. Authenticate
-scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-creds_dict = json.loads(SERVICE_ACCOUNT_JSON)
-creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
-gc = gspread.authorize(creds)
+    if not service_account_json:
+        raise ValueError("GOOGLE_SERVICE_ACCOUNT_JSON environment variable is missing.")
 
-# 3. Open Spreadsheet and Target Sheet by GID
-spreadsheet = gc.open_by_key(SPREADSHEET_ID)
-worksheet = spreadsheet.get_worksheet_by_id(int(WORKSHEET_GID))
+    # Authenticate
+    scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+    creds_dict = json.loads(service_account_json)
+    creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+    gc = gspread.authorize(creds)
+
+    # Open Spreadsheet & Target Worksheet
+    spreadsheet = gc.open_by_key(spreadsheet_id)
+    
+    if worksheet_gid:
+        worksheet = spreadsheet.get_worksheet_by_id(int(worksheet_gid))
+    else:
+        worksheet = spreadsheet.get_worksheet(0)
+
+    # Clear old data and write the new DataFrame
+    worksheet.clear()
+    set_with_dataframe(worksheet, df, include_index=False, include_column_header=True)
+    return "Data successfully synced to Google Sheets!"
